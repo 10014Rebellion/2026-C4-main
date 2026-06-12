ea508055534f28b967630b4b77df36791c7c373c
RebellionGit10014 <112141918+ARMdevx@users.noreply.github.com>
2026-06-11 20:48:47 -0500

    wait time changed from 1 to .5 sec

--- src/main/java/frc/robot/systems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/systems/intake/IntakeConstants.java
@@ -49,7 +49,7 @@ public class IntakeConstants {
                 public static double kRackToleranceMeters = Units.inchesToMeters(0.25);
                 public static double kRollerUsageCutoffMeters = 0.07;
                 // public static double kRackTolerance = 0.05;
-                public static double kRackStowDelay = 1; // Seconds
+                public static double kRackStowDelay = 0.5; // Seconds
 
                 public static final BasicMotorHardware kRackMotorConfig = new BasicMotorHardware(
                                 44, // TODO: TUNE ME;
